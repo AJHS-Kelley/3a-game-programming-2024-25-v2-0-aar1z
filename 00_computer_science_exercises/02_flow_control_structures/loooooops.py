9b6c165aca035e8a4b85f4adaee94944157d8786
aar1z <s11856093@students.duvalschools.org>
2024-09-06 12:57:58 -0400

    while looooooops

--- 00_computer_science_exercises/02_flow_control_structures/loooooops.py
+++ 00_computer_science_exercises/02_flow_control_structures/loooooops.py
@@ -1,4 +1,6 @@
 # Loops, Kristin Taylor, v0.0
+import random # import the random module for us to use.
+# Generally put all your import statement at the top.
 
 # TWO TYPES OF LOOPS
 # for <-- Used when you know how many loops you'll need.
@@ -11,24 +13,43 @@
 # iterate through the list means use a for loop
 
 # for loops Example -- Iterating a list
-fruits = ["apple", "banana", "strawberry", "tomato"]
-for eachFruit in fruits:
-    print(eachFruit)
+#fruits = ["apple", "banana", "strawberry", "tomato"]
+#for eachFruit in fruits:
+    #print(eachFruit)
 
 # continue Keyword -- Skips the current iteration and the finishes the loop.
-    fruits = ["apple", "banana", "strawberry", "tomato"]
-for eachFruit in fruits:
-    if eachFruit == "banana":
-     continue
-    print(eachFruit)
+    #fruits = ["apple", "banana", "strawberry", "tomato"]
+#for eachFruit in fruits:
+    #if eachFruit == "banana":
+     #continue
+    #print(eachFruit)
 
 # break Keyword -- Immediately exits the loop.
-fruits = ["apple", "banana", "strawberry", "tomato"]
-for eachFruit in fruits:
-    if eachFruit == "banana":
-     break
-    print(eachFruit)
+#fruits = ["apple", "banana", "strawberry", "tomato"]
+#for eachFruit in fruits:
+    #if eachFruit == "banana":
+     #break
+    #print(eachFruit)
 
 # for loops using range(). range(x) is EXCLUSIVE, it starts at 0 and ends at -1
 for i in range(10): # range is 0 - 9
    print(i)
+
+# Might not always want to start at 0.
+for i in range(10, 100): #
+   print(i)
+
+# Not want to count by 1
+for i in range( 10, 100, 5): # 10 = start, 100 - 1 = stop, 5 = # to count by.
+   print(i)
+
+# Sometimes you're not done writing loops.
+for x in range(10):
+   pass # tells Pythons this loops isn't finished, dont freak out.
+
+# while loops -- Musical chairs
+playerScore = 0
+while playerScore < 100: # Run as long as this is True.
+   print(f"Starting: {playerScore}")
+   playerScore += random.randint(1,3)
+   print(f"After: {playerScore}")
