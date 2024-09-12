93b97dcb60f048d10e740b5ad07d620005c56283
aar1z <s11856093@students.duvalschools.org>
2024-09-12 12:43:18 -0400

    k

--- 01_gaming_exercises/00_awarding_extra_lives/awardingextralives.py
+++ 01_gaming_exercises/00_awarding_extra_lives/awardingextralives.py
@@ -1,7 +1,7 @@
 # Awarding Extra Lives, Kristin Taylor v0.0
 
 lives = 3
-
+score = 10001
 # Allows the user to input the score AS A INTEGER.
 
 # If lives is 10000 or less
@@ -13,9 +13,9 @@ lives = 3
 
 # Output the score amd number of lives to the screen.
 
-If lives < 10000
-    print = ("Lose a life")
-If score > 10000 but less than 10001
-    print = ("Give 1 extra life")
-If score is > 10000
-    print = ("Give 2 extra Lives")
+if score > 10000:
+   print = ("Lose a life")
+if score <= 10001: 
+   print = ("Give 1 extra life")
+if score > 10000:
+   print = ("Give 2 extra Lives")
