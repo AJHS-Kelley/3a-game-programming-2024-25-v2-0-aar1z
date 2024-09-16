e5cf6f482f04c3b48700667f45f97be366d647a9
Ryan Kelley <kelleyr@duvalschools.org>
2024-09-16 12:54:12 -0400

    Update awardingextralives.py

--- 01_gaming_exercises/00_awarding_extra_lives/awardingextralives.py
+++ 01_gaming_exercises/00_awarding_extra_lives/awardingextralives.py
@@ -6,16 +6,17 @@ name = "kristin"
 
 print(f"Hello {name}!  You scored {score} points.\n")
 
-# Allows the user to input the score AS A INTEGER.
+# Allows the user to input the score AS A INTEGER. <-- YOU ARE MISSING THIS PART OF THE CODE.  
 
 # Output the score amd number of lives to the screen.
 
 if score <= 10000:
    print("Lose a life")
-elif score < 10001: 
+   # Did not remove a life.  
+elif score < 100001: # You put 10001 the first time.  Be careful about the values! 
    print("Give 1 extra life")
    lives += 1
-elif score > 100000:
+elif score > 100000: # This block should be an else:  
    print("Give 2 extra Lives")
    lives += 2
 
