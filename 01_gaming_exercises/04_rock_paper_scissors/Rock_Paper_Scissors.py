06e2c846fbf92ef7a890b623111a8d106288dce7
aar1z <s11856093@students.duvalschools.org>
2024-09-18 12:04:11 -0400

    else

--- 01_gaming_exercises/04_rock_paper_scissors/Rock_Paper_Scissors.py
+++ 01_gaming_exercises/04_rock_paper_scissors/Rock_Paper_Scissors.py
@@ -3,13 +3,20 @@
 # MODULE IMPORTS
 import random
 
-#DATA STRUCTURES -- PLAYER
-
+# DATA STRUCTURES -- PLAYER
 playerScore=0
 playerName="Test Player"
 playerChoice=None
-CPUscore=0
 
 # DATA STRUCTURES -- CPU
 CPUscore=0
 CPUscore=None
+
+# PLAYER NAME INPUT
+playerName = input("Type your name and press enter.\n")
+print(f"Hello{playerName}!\n")
+isCorrect = input("Is that correct? Type yes or no and press enter.\n").lower()
+if isCorrect == "yes":
+    print(f"Ok {playerName}, let;s play Rock, Paper, Scissors!\n")
+else:
+    playerName = input("Type your name and press enter.\n")
