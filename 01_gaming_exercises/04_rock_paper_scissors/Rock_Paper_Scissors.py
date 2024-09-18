16a0243c81b8e8acd6c3dd44ca0606236c747ac4
aar1z <s11856093@students.duvalschools.org>
2024-09-18 11:49:47 -0400

    CPU

---
+++ 01_gaming_exercises/04_rock_paper_scissors/Rock_Paper_Scissors.py
@@ -0,0 +1,15 @@
+# Rock paper scissors, Kristin Taylor v0.1
+
+# MODULE IMPORTS
+import random
+
+#DATA STRUCTURES -- PLAYER
+
+playerScore=0
+playerName="Test Player"
+playerChoice=None
+CPUscore=0
+
+# DATA STRUCTURES -- CPU
+CPUscore=0
+CPUscore=None
